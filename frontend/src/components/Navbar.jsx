816695d89fbf4e1e5504ab7f921d8cade7d8bf42
JustusWamswa <justuswamswa@gmail.com>
2023-05-07 03:17:57 +0400

    initial commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="w-24 h-24 bg-[url('./assets/logo.png')] bg-center bg-contain bg-no-repeat">
+    </div>
+  )
+}
+
+export default Navbar
